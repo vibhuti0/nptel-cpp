b79c39a0895ead88755bd0126dea0e844ee9d125
Vibhuti Joshi <vp1373@srmist.edu.in>
2023-07-28 21:48:05 +0530

    third commit

---
+++ sqrt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double x;
+    cout<<"enter a number to find square root\n";
+    cin>>x;
+    double sqrt_x=sqrt(x);
+    cout<<"the square root of"<<x<<"is"<<sqrt_x;
+    cout<<endl;
+    return 0;
+}
