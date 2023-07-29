b483731e1cb8261fc42a0ead59677c53d0c23d04
Vibhuti Joshi <vp1373@srmist.edu.in>
2023-07-29 21:29:14 +0530

    commit

---
+++ concat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(void)
+{
+    string str1="welcome ";
+    string str2="vibhuti";
+    
+    cout<<str1.append(str2);
+    return 0;
+}
