d393dc29b2c83107c786cde4b3688f7af50ca2f0
Vibhuti Joshi <vp1373@srmist.edu.in>
2023-08-06 10:33:28 +0530

    commit

---
+++ q8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+char fun(int a,char b)
+{
+    char c=a+b;
+    return c;
+}
+char fun(char a,int b)
+{
+    char c=a-b;
+    return c;
+}
+int main()
+{
+    cout<<fun(100,10);
+    return 0;
+}
+//ambiguous call of (int,char)
