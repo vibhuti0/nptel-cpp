1f611d3681e2e459fc8f2d180d0dce4fb20c26bb
Vibhuti Joshi <vp1373@srmist.edu.in>
2023-08-14 09:43:30 +0530

    commit

---
+++ week1,2/week3/q3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class private_data
+{
+    int x;
+    void f1()
+    {
+        cout<<"within f1";
+    }
+    public:
+    int y;
+    void f2();
+    {
+        cout<<"within f2;"
+    }
+};
+int main()
+{
+    private_data t;
+    t.x=1;
+    t.f1();
+    t.y=2;
+    t.f2();
+    return 0;
+}
